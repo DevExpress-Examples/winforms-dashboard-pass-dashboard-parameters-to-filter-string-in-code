4e977de007e4c04af690dd38bfada953297fcd5a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_Parameters/Form1.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.DashboardCommon;
+
+namespace Dashboard_Parameters {
+    public partial class Form1 : XtraForm {
+        public Form1() {
+            InitializeComponent();
+
+            // Loads a dashboard from an XML file.
+            Dashboard dashboard = new Dashboard();
+            dashboard.LoadFromXml(@"..\..\Data\Dashboard.xml");
+
+            // Obtains dashboard items and specifies idenifiers for required data items.
+            GridDashboardItem grid = (GridDashboardItem)dashboard.Items[0];
+            PieDashboardItem pie = (PieDashboardItem)dashboard.Items[1];  
+            ((GridDimensionColumn)grid.Columns[0]).Dimension.ID = "categoryColumn";
+            pie.SeriesDimensions[0].ID = "categorySeries";
+
+            // Obtains the dashboard data source used to provide parameter values.
+            DashboardSqlDataSource parameterDataSource = 
+                (DashboardSqlDataSource)dashboard.DataSources[0];
+
+            // Creates a new parameter that obtains its values from the Categories query.
+            DynamicListLookUpSettings settings = new DynamicListLookUpSettings();
+            settings.DataSource = parameterDataSource;
+            settings.DataMember = "Categories";
+            settings.ValueMember = "CategoryName";
+            settings.DisplayMember = "CategoryName";
+            DashboardParameter parameter = 
+                new DashboardParameter("Category", typeof(string), "Beverages", "", true, settings);
+
+            // Adds the created parameter to a collection of dashboard parameters.
+            dashboard.Parameters.Add(parameter);
+
+            // Filters the Grid and Pie dashboard items according to the specified parameter.
+            grid.FilterString = "categoryColumn = ?Category";
+            pie.FilterString = "categorySeries = ?Category";
+
+            dashboardViewer1.Dashboard = dashboard;
+        }
+    }
+}
---
+++ VB/Dashboard_Parameters/Form1.vb
@@ -0,0 +1,44 @@
+﻿Imports DevExpress.XtraEditors
+Imports DevExpress.DashboardCommon
+
+Namespace Dashboard_Parameters
+    Partial Public Class Form1
+        Inherits XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+
+            ' Loads a dashboard from an XML file.
+            Dim dashboard As New Dashboard()
+            dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
+
+            ' Obtains dashboard items and specifies idenifiers for required data items.
+            Dim grid As GridDashboardItem = CType(dashboard.Items(0), GridDashboardItem)
+            Dim pie As PieDashboardItem = CType(dashboard.Items(1), PieDashboardItem)
+            CType(grid.Columns(0), GridDimensionColumn).Dimension.ID = "categoryColumn"
+            pie.SeriesDimensions(0).ID = "categorySeries"
+
+            ' Obtains the dashboard data source used to provide parameter values.
+            Dim parameterDataSource As DashboardSqlDataSource =
+                CType(dashboard.DataSources(0), DashboardSqlDataSource)
+
+            ' Creates a new parameter that obtains its values from the Categories query.
+            Dim settings As New DynamicListLookUpSettings()
+            settings.DataSource = parameterDataSource
+            settings.DataMember = "Categories"
+            settings.ValueMember = "CategoryName"
+            settings.DisplayMember = "CategoryName"
+            Dim parameter As New DashboardParameter("Category", GetType(String), _
+                                                    "Beverages", "", True, settings)
+
+            ' Adds the created parameter to a collection of dashboard parameters.
+            dashboard.Parameters.Add(parameter)
+
+            ' Filters the Grid and Pie dashboard items according to the specified parameter.
+            grid.FilterString = "categoryColumn = ?Category"
+            pie.FilterString = "categorySeries = ?Category"
+
+            dashboardViewer1.Dashboard = dashboard
+        End Sub
+    End Class
+End Namespace
