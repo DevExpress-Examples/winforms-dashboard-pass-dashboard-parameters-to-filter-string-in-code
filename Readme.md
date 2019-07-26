c3c86e15b1a5424b34728e5920b59c5796f9783f
BrianDX <boris.zaitsev@devexpress.com>
2019-07-26 19:45:21 +0300

    Update 19.1.3+

--- Readme.md
+++ Readme.md
@@ -6,10 +6,23 @@
 # How to pass a dashboard parameter to a filter string in code
 
 
-<p>The following example demonstrates how to <a href="http://documentation.devexpress.com/#Dashboard/CustomDocument16169">create a new dashboard parameter</a> and <a href="http://documentation.devexpress.com/#Dashboard/CustomDocument16170">pass</a> it to a dashboard item filter string.</p>
-<p>In this example, the dashboard data source contains two queries - the SalesPerson query is used for data visualization while the Categories query provides values for the dashboard parameter.</p>
-<p>After the dashboard parameter is created, it is passed to a dashboard item's filter strings. So, the dashboard displays data according to the selected values.</p>
+This example demonstrates how to [create a new dashboard parameter](https://docs.devexpress.com/Dashboard/16169) and [pass](https://docs.devexpress.com/Dashboard/16170) it to a dashboard item filter string.
 
-<br/>
 
+In this example, the dashboard data source contains two queries - the **SalesPerson** query is used for data visualization while the **Categories** query provides values for the dashboard parameter.
+
+The dashboard parameter is included into dashboard item's filter expressions. Thus parameter selection forces the dashboard to display data according to the selected values.
+
+![](/images/screenshot.png)
+
+API in this example:
+
+* [DashboardParameter ](https://docs.devexpress.com/Dashboard/DevExpress.DashboardCommon.DashboardParameter) class
+* [DynamicListLookUpSettings](https://docs.devexpress.com/Dashboard/DevExpress.DashboardCommon.DynamicListLookUpSettings) class
+* [Dashboard.Parameters](https://docs.devexpress.com/Dashboard/DevExpress.DashboardCommon.Dashboard.Parameters) property
+* [DataDashboardItem.FilterString](https://docs.devexpress.com/Dashboard/DevExpress.DashboardCommon.DataDashboardItem.FilterString) property
+
+See also:
+
+* [Member Table: Dashboard Parameters](https://docs.devexpress.com/Dashboard/17505)
 
--- VB/Dashboard_Parameters/Form1.vb
+++ VB/Dashboard_Parameters/Form1.vb
@@ -2,44 +2,40 @@
 Imports DevExpress.DashboardCommon
 
 Namespace Dashboard_Parameters
-    Partial Public Class Form1
-        Inherits XtraForm
+	Partial Public Class Form1
+		Inherits XtraForm
 
-        Public Sub New()
-            InitializeComponent()
+		Public Sub New()
+			InitializeComponent()
+			Dim dashboard As New Dashboard()
+			dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
 
-            ' Loads a dashboard from an XML file.
-            Dim dashboard As New Dashboard()
-            dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
+			' Obtain dashboard items and specify identifiers for data items.
+			Dim grid As GridDashboardItem = CType(dashboard.Items(0), GridDashboardItem)
+			Dim pie As PieDashboardItem = CType(dashboard.Items(1), PieDashboardItem)
+			CType(grid.Columns(0), GridDimensionColumn).Dimension.UniqueId = "categoryColumn"
+			pie.SeriesDimensions(0).UniqueId = "categorySeries"
 
-            ' Obtains dashboard items and specifies idenifiers for required data items.
-            Dim grid As GridDashboardItem = CType(dashboard.Items(0), GridDashboardItem)
-            Dim pie As PieDashboardItem = CType(dashboard.Items(1), PieDashboardItem)
-            CType(grid.Columns(0), GridDimensionColumn).Dimension.UniqueId = "categoryColumn"
-            pie.SeriesDimensions(0).UniqueId = "categorySeries"
+			' Obtain the dashboard data source used to provide parameter values.
+			Dim parameterDataSource As DashboardSqlDataSource = DirectCast(dashboard.DataSources(0), DashboardSqlDataSource)
 
-            ' Obtains the dashboard data source used to provide parameter values.
-            Dim parameterDataSource As DashboardSqlDataSource =
-                CType(dashboard.DataSources(0), DashboardSqlDataSource)
+			' Create a new parameter that obtains its values from the Categories query.
+			Dim settings As New DynamicListLookUpSettings()
+			settings.DataSource = parameterDataSource
+			settings.DataMember = "Categories"
+			settings.ValueMember = "CategoryName"
+			Dim parameter As New DashboardParameter("categoryParameter", GetType(String), "Beverages", "Select categories:", True, settings)
+			' Enable multi-selection for the created parameter.
+			parameter.AllowMultiselect = True
 
-            ' Creates a new parameter that obtains its values from the Categories query.
-            Dim settings As New DynamicListLookUpSettings()
-            settings.DataSource = parameterDataSource
-            settings.DataMember = "Categories"
-            settings.ValueMember = "CategoryName"
-            Dim parameter As New DashboardParameter("categoryParameter", GetType(String), _
-                                                    "Beverages", "Select categories:", True, settings)
-            ' Enables multi-selection for the created parameter.
-            parameter.AllowMultiselect = True
+			' Add the created parameter to a collection of dashboard parameters.
+			dashboard.Parameters.Add(parameter)
 
-            ' Adds the created parameter to a collection of dashboard parameters.
-            dashboard.Parameters.Add(parameter)
+			' Include the created parameter in filter strings as an operand value.
+			grid.FilterString = "categoryColumn in (?categoryParameter)"
+			pie.FilterString = "categorySeries in (?categoryParameter)"
 
-            ' Includes the created parameter in filter strings as an operand value.
-            grid.FilterString = "categoryColumn in (?categoryParameter)"
-            pie.FilterString = "categorySeries in (?categoryParameter)"
-
-            dashboardViewer1.Dashboard = dashboard
-        End Sub
-    End Class
+			dashboardViewer1.Dashboard = dashboard
+		End Sub
+	End Class
 End Namespace
